e0afefdf8c3a147044050ac3a44984ff7181eafb
Nick Nelson <nick@chrnnk.tv>
2024-10-21 19:02:55 -0400

    Create tailscale-add-to-unprivileged-lxc.sh

---
+++ tailscale-add-to-unprivileged-lxc.sh
@@ -0,0 +1,3 @@
+# add the following to the lxc.conf file at /etc/pve/lxc/100.conf for example
+lxc.cgroup2.devices.allow: c 10:200 rwm
+lxc.mount.entry: /dev/net/tun dev/net/tun none bind,create=file
