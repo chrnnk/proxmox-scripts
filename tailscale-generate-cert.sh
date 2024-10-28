0a345afa69d33ee72e27995c531f82cf889ecc8a
Nick Nelson <nick@chrnnk.tv>
2024-10-28 17:24:30 -0400

    Create tailscale-generate-cert.sh

---
+++ tailscale-generate-cert.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt install jq -y
+NAME="$(tailscale status --json | jq '.Self.DNSName | .[:-1]' -r)"
+tailscale cert "${NAME}"
+pvenode cert set "${NAME}.crt" "${NAME}.key" --force --restart
