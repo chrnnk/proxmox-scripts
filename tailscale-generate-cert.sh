45fac190ecd64c3e34f12ae60b86019a5b5b9598
Nick Nelson <nick@chrnnk.tv>
2024-10-28 17:28:24 -0400

    Update tailscale-generate-cert.sh

--- tailscale-generate-cert.sh
+++ tailscale-generate-cert.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+te#!/bin/bash
 apt install jq -y
 NAME="$(tailscale status --json | jq '.Self.DNSName | .[:-1]' -r)"
 tailscale cert "${NAME}"
 pvenode cert set "${NAME}.crt" "${NAME}.key" --force --restart
 # run crontab -e and add this line
-# 0 0 * * 0 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin /root/tailscale-cert.sh
+# 0 0 * * 0 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin /root/tailscale-generate-cert.sh
